584253490f2acd8e717bd87150c6dd6074d3c1cf
Kenny Cohen <kicohen@andrew.cmu.edu>
2018-02-21 20:48:59 -0500

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ActiveRecord::Base
+	belongs_to :category
+	 validates_presence_of :title
+	  validates_presence_of :content
+  scope :active, where('active = ?', true)
+  scope :alphabetical, order('title')
 end
